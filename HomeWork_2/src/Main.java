33b4965b0485c8fd01890cf5710d2f7fb4b06b6c
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2022-03-20 13:23:18 +0200

    Выведение форматированной информации
    Вариант 5

---
+++ HomeWork_2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nameComp = "Apple";
+        double annualIncomeComp = 274.5;
+        int marketShare = 46;
+
+        System.out.printf("Название компании: %s; Годовой доход: %.1f; Доля на Российском рынке смартфонов: %d" ,
+                            nameComp, annualIncomeComp, marketShare);
+
+    }
+}
