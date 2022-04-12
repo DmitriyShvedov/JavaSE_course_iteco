e061f727aeaa66b893e90eac89a4104b3d1fd626
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2022-04-12 16:31:11 +0200

    HM_5 v.0.1.1

--- HomeWork_5/src/main.java
+++ HomeWork_5/src/main.java
@@ -7,34 +7,72 @@ public class main {
 
     public static void main(String[] args) {
 
-        String temp = "Дмитрий 2";
-        System.out.println("Введите Имя и набор цифр для подсчета суммы(напр. Максим 711)");
+        System.out.println("INPUT NAME AND NUMBERS (EX: Максим 711 )");
         Scanner scr = new Scanner(System.in);
-        String temp2 = scr.nextLine();
-        if (checkWordAndValues(temp2)){
-            do {
-                System.out.println("Введите Имя и набор цифр(напр. Максим 711)");
-                temp2 = scr.nextLine();
+        String temp = scr.nextLine();
 
-            }while (checkWordAndValues(temp2));
+        String[] words = temp.split(" ");
+
+        while (!(checkNumbers(words) & checkName(words))) {
+            temp = scr.nextLine();
+            words = temp.split(" ");
         }
 
-
-        String[] words = temp2.split(" ");
-        String[] values = words[1].split("");
         String firstName = words[0];
+        String[] values = words[1].split("");
 
         int[] valueInt = new int[values.length];
 
-        for (int i = 0; i < values.length; i++) {
-            valueInt[i] = Integer.parseInt(values[i]);
-            System.out.println(valueInt[i]);
+        parsStrToInt(values, valueInt);
+
+        while (values.length < 2 || values.length > 5) {
+            System.out.println("INPUT ONE MORE YOUR NUMBERS");
+            temp = scr.nextLine();
+            values = temp.split("");
         }
 
-        int summ = summValues(valueInt);
-        String nameSummNumber = converterIntToString(summ);
+        valueInt = new int[values.length];
+
+        parsStrToInt(values, valueInt);
+
+        System.out.print("YOUR INPUT DATA: " + firstName + " ");
+        for (String value : values) {
+            System.out.print(value);
+        }
+
+        String nameSummNumber = converterIntToString(summValues(valueInt));
         greetings(firstName, nameSummNumber);
 
+    }
+
+    public static int[] parsStrToInt(String[] values, int[] valueInt) {
+        for (int i = 0; i < values.length; i++) {
+            valueInt[i] = Integer.parseInt(values[i]);
+        }
+        return valueInt;
+    }
+
+    public static Boolean checkNumbers(String[] words) {
+        if (words.length != 2) {
+            System.out.println("NO NUMBERS! REPEAT INPUT NAME AND NUMBERS");
+            return false;
+        }
+        return true;
+    }
+
+    public static Boolean checkName(String[] words) {
+        if (words[0].isEmpty()) {
+            System.out.println("NO NAME! REPEAT INPUT NAME AND NUMBERS! ");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static Boolean checkSummValues(int[] array) {
+
+        int check = summValues(array);
+        return check > 10;
     }
 
 
@@ -50,72 +88,51 @@ public class main {
         }
         return result;
     }
-    public static int checkSummValues(int[] array) {
-
-        if (i >= array.length && result>10)
-            return result;
-        else {
-            int temp = array[i];
-            result += temp;
-            i++;
-            summValues(array);
-        }
-        return result;
-    }
-
-
 
     public static void greetings(String name, String summ) {
-        System.out.println("Здравствуй, " + name + "! Сумма чисел в числе = " + summ);
+        System.out.println("\n" + "Здравствуй, " + name + "! Сумма чисел в числе = " + summ);
     }
 
-    public static String converterIntToString(int num){
+    public static String converterIntToString(int num) {
         String temp;
-        switch (num){
-            case 1 :
+        switch (num) {
+            case 1:
                 temp = "один";
                 break;
-            case 2 :
+            case 2:
                 temp = "два";
                 break;
-            case 3 :
+            case 3:
                 temp = "три";
                 break;
-            case 4 :
+            case 4:
                 temp = "четыре";
                 break;
-            case 5 :
+            case 5:
                 temp = "пять";
                 break;
-            case 6 :
+            case 6:
                 temp = "шесть";
                 break;
-            case 7 :
+            case 7:
                 temp = "семь";
                 break;
-            case 8 :
+            case 8:
                 temp = "восемь";
                 break;
-            case 9 :
+            case 9:
                 temp = "девять";
                 break;
-            case 10 :
+            case 10:
                 temp = "десять";
                 break;
 
-                default:
-                    temp = "Ошибка в подчетах, либо сумма больше 10";
+            default:
+                temp = "Ошибка в подчетах, либо сумма больше 10";
 
         }
         return temp;
-    };
+    }
 
-    public static Boolean checkWordAndValues(String str){
-        String[] words = str.split(" ");
-         if ((words[0] != null & !words[0].isEmpty()) & (!words[1].isEmpty() && words[1] != null)){
-             return false;
-         }else
-             return true;
 
-    };
 }
