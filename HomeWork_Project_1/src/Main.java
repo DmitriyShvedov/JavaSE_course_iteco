3ee9339ae8e1acad8010061d12af74c1d5a860b5
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2022-04-15 14:28:17 +0200

    + метод сложения + метод парса

--- HomeWork_Project_1/src/Main.java
+++ HomeWork_Project_1/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
+        String num1 = "-2 + 1";
+        String[] tempArray = num1.split(" ");
+
+        int[] afteParsToIntArray = parsStringToInt(tempArray);
+
+        System.out.println(addNumbers(afteParsToIntArray));
+
     }
+
+
+
+    public static int addNumbers(int[] array){
+        int result = 0;
+        int a = array[0];
+        int b = array[1];
+        return result = a + b;
+    }
+
+    public static int[] parsStringToInt(String[] array){
+        int[] numberDoubl = new int[array.length-1];
+        numberDoubl[0] = Integer.parseInt(array[0]);
+        numberDoubl[1] = Integer.parseInt(array[2]);
+        return numberDoubl;
+    }
+
 }
