5b279d351cd84d8ae4cf5d6ac6070619e6fa9345
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2022-04-15 14:58:09 +0200

    + методы вычитания и деления

--- HomeWork_Project_1/src/Main.java
+++ HomeWork_Project_1/src/Main.java
@@ -1,23 +1,36 @@
 public class Main {
     public static void main(String[] args) {
 
-        String num1 = "-2 + 1";
+        String num1 = "15 / -4";
         String[] tempArray = num1.split(" ");
 
-        int[] afteParsToIntArray = parsStringToInt(tempArray);
+        int[] afterParsToIntArray = parsStringToInt(tempArray);
 
-        System.out.println(addNumbers(afteParsToIntArray));
+        System.out.println(addNumbers(afterParsToIntArray));
+        System.out.println(subtrNumbers(afterParsToIntArray));
+        System.out.println(divNumbers(afterParsToIntArray));
 
     }
 
 
 
     public static int addNumbers(int[] array){
-        int result = 0;
         int a = array[0];
         int b = array[1];
-        return result = a + b;
+        return a + b;
     }
+    public static int subtrNumbers(int[] array){
+        int a = array[0];
+        int b = array[1];
+        return a - b;
+    }
+    public static float divNumbers(int[] array){
+        int a = array[0];
+        int b = array[1];
+        return (float) a/b;
+    }
+
+
 
     public static int[] parsStringToInt(String[] array){
         int[] numberDoubl = new int[array.length-1];
