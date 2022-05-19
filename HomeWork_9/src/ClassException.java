3fbf0dc3fb057dd247411fdf20c4ce3b73e6ac1f
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2022-05-19 16:50:42 +0200

    добавил обработку ошибки зациклил пока будет не верная строка

--- HomeWork_9/src/ClassException.java
+++ HomeWork_9/src/ClassException.java
@@ -1,6 +1,24 @@
 public class ClassException extends NumberFormatException {
 
-    public ClassException(String massage) {
-        super(massage);
+    private int num;
+    private char str;
+
+    public char getStr() {
+        return str;
     }
+
+    public int getNum() {
+        return num;
+    }
+
+    public ClassException(String massage, int a) {
+        super(massage);
+        this.num = a;
+    }
+
+    public ClassException(String massage, char a) {
+        super(massage);
+        this.str = a;
+    }
+
 }
--- HomeWork_9/src/Main.java
+++ HomeWork_9/src/Main.java
@@ -6,34 +6,41 @@ public class Main {
 
     static int intV;
     static String srtV;
+    static boolean flag;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.next();
-        char[] c = str.toCharArray();
-        equalsValue(c);
+        do {
+            System.out.println("INPUT STR");
+            Scanner scanner = new Scanner(System.in);
+            String str = scanner.next();
+            char[] c = str.toCharArray();
+            equalsValue(c);
+        }while(!flag);
+
 
     }
 
     public static boolean equalsValue(char[] mass) {
-        for (int i = 0; i < mass.length - 1; i++) {
-            if (Objects.equals(mass[i], mass[i+1]) && Objects.equals(mass[i], mass[i+2])) {
-                System.out.println(mass[i]);
-                return true;
+        try {
+            for (int i = 0; i < mass.length - 1; i++){ //sdfsdf44 - не пашет
+                if (Objects.equals(mass[i], mass[i + 1]) && Objects.equals(mass[i], mass[i + 2]))
+                    throw new ClassException("INPUT AGAIN. REPEED: ", mass[i]);
+                }
+            return searchValue(mass);
+            }catch(ClassException ce){
+                System.out.print(ce.getMessage());
+                System.out.println(ce.getStr());
             }
-        }
-        System.out.println("повторов нет");
-
-        return searchValue(mass);
+        return flag = false;
     }
 
     public static boolean searchValue(char[] mass) {
         for(int i = 0; i < mass.length; i ++)
         if (Character.isDigit(mass[i])){
             System.out.println(mass[i]);
-            return true;
+            return flag = false;
         }
-        return false;
+        return flag = true;
         }
 
 }
